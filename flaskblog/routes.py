b9fe0928006cfcd6569345a87b1f2c0a7a54f132
Andrii <146644879+bilyi1andrii@users.noreply.github.com>
2024-03-14 08:12:19 +0200

    Update routes.py

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,6 +1,6 @@
 import os
 import secrets
-# from PIL import Image
+from PIL import Image
 from flask import render_template, url_for, flash, redirect, request, abort
 from flask_login import login_user, current_user, logout_user, login_required
 from flaskblog import app, db, bcrypt
