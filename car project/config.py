a218bc7dd5fdd6e131a6411b368001c52b0cb757
Andrii <146644879+bilyi1andrii@users.noreply.github.com>
2024-03-27 13:14:30 +0200

    Update config.py

--- car project/config.py
+++ car project/config.py
@@ -1,1 +1,9 @@
-
+class Config:
+    SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    API_KEY = "AIzaSyDX3U8lyUMgE_-RQM9kGL0TAORBZWjTVJs"
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = "noreplier228@gmail.com"
+    MAIL_PASSWORD = "ounu fmcu lbxr fwgl"
